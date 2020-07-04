800e64cd27d5778b46b1d2e49fcfe1add8bc385a
Alexandr <Alokil70@gmail.com>
2020-07-04 19:04:58 +0300

    add client

--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,47 @@
-<template> </template>
+<template>
+    <div>
+        <h1>Войти</h1>
+        <div class="form">
+            <div class="form-group form-w50">
+                <label for="login">Логин</label>
+                <input v-model="login.user" type="text" name="login" />
+            </div>
+            <div class="form-group form-w50">
+                <label for="password">Пароль</label>
+                <input v-model="login.password" type="password" name="login" />
+            </div>
+            <div class="form-group form-w50">
+                <input type="submit" value="Войти" @click="userLogin" />
+            </div>
+        </div>
+    </div>
+</template>
 
 <script>
 export default {
-    name: 'Login'
+    name: 'Login',
+    data: () => ({
+        login: {
+            user: '',
+            password: ''
+        }
+    }),
+    methods: {
+        async userLogin() {
+            try {
+                const response = await this.$auth.loginWith('local', {
+                    data: this.login
+                })
+            } catch(e) {
+                console.log(e)
+            }
+        }
+    },
+    head() {
+        return {
+            meta: [{ name: "robots", content: "noindex,nofollow"}]
+        }
+    }
 }
 </script>
 
